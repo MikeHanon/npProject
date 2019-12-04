c880579efad34e3fb8b2b357b87ef7a44a6b58ce
mike <mike.hanon@gmail.com>
2019-12-04 12:28:58 +0100

    suite register

--- class/ClassUser.php
+++ class/ClassUser.php
@@ -38,4 +38,50 @@ class USER
         }
 
     }
+
+    public function login($username, $password)
+    {
+        try
+        {
+            $stmt = $this->db->prepare("SELECT * FROM user WHERE username = :username LIMIT 1");
+            $stmt->execute(array(':username'=>$username));
+            $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+            if($stmt->rowCount()>0)
+            {
+                if(password_verify($password,$userRow['password']))
+                {
+                    $_SESSION['user_session']= $userRow['username'];
+                    return true;
+                }else
+                {
+                    return false;
+                }
+            }
+        }
+        catch(PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+
+    }
+
+    public function is_loggedin()
+    {
+        if(isset($_SESSION['user_session']))
+        {
+            return true;
+        }
+    }
+
+    public function redirect($url)
+    {
+        header("location: $url");
+    }
+    public function logout()
+    {
+        session_destroy();
+        unset($_SESSION['user_session']);
+        return true;
+    }
 }
+?>
